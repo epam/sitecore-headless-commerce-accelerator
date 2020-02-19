bbcdeedc49863204d347bd0ba04e1f79573a8289
Daria Larionova <daria_larionova@epam.com>
2020-02-19 19:47:26 +0300

    Add configuration provider and service

---
+++ src/Foundation/Base/code/Services/Configuration/ConfigurationProvider.cs
@@ -0,0 +1,30 @@
+﻿namespace Wooli.Foundation.Base.Services.Configuration
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using Models.Configuration;
+    using Sitecore.Configuration;
+
+    [ExcludeFromCodeCoverage]
+    public abstract class ConfigurationProvider<TConfiguration> : IConfigurationProvider<TConfiguration> where TConfiguration : Configuration, new()
+    {
+        private readonly Lazy<TConfiguration> configuration;
+        protected abstract string Path { get; set; }
+
+        protected ConfigurationProvider()
+        {
+            this.configuration = new Lazy<TConfiguration>(this.Read, true);
+        }
+
+        public TConfiguration Get()
+        {
+            return this.configuration.Value;
+        }
+
+        private TConfiguration Read()
+        {
+            var xmlNode = Factory.GetConfigNode(this.Path);
+            return xmlNode != null ? Factory.CreateObject<TConfiguration>(xmlNode) : null;
+        }
+    }
+}
---
+++ src/Foundation/Base/code/Services/Configuration/ConfigurationService.cs
@@ -0,0 +1,16 @@
+﻿namespace Wooli.Foundation.Base.Services.Configuration
+{
+    using System.Diagnostics.CodeAnalysis;
+    using DependencyInjection;
+    using Microsoft.Extensions.DependencyInjection;
+    using Models.Configuration;
+    using Sitecore.DependencyInjection;
+
+    [ExcludeFromCodeCoverage]
+    [Service(typeof(IConfigurationService))]
+    public class ConfigurationService : IConfigurationService
+    {
+        public TConfiguration Get<TConfiguration>() where TConfiguration : Configuration, new() 
+            => ServiceLocator.ServiceProvider.GetService<IConfigurationProvider<TConfiguration>>().Get();
+    }
+}

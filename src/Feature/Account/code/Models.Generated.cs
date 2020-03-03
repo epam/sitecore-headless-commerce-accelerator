3e11cba6b57daf9fcace18edd063884db70d2a44
Evgenii Nikulin <Evgenii_Nikulin@epam.com>
2020-03-03 16:32:55 +0300

    Fix copyrighting in tamplates

--- src/Feature/Account/code/Models.Generated.cs
+++ src/Feature/Account/code/Models.Generated.cs
@@ -1,4 +1,4 @@
-//    Copyright 2019 EPAM Systems, Inc.
+//    Copyright 2020 EPAM Systems, Inc.
 // 
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
@@ -39,6 +39,6 @@ namespace Wooli.Feature.Account.Models
     using Sitecore.Data.Items;
  
     using System.CodeDom.Compiler;
-    using Epam.Sc.EngX.CodeGeneration.Domain;
+    using Wooli.Foundation.GlassMapper.Models;
     
 }
--- src/Feature/Account/code/codegeneration.config.js
+++ src/Feature/Account/code/codegeneration.config.js
@@ -3,6 +3,6 @@ var path = require("path");
 module.exports = {
     cwd: path.join(__dirname, '..'),
     pattern: '**/serialization/Templates/**/*.yml',
-    Using: ['System.CodeDom.Compiler', 'Epam.Sc.EngX.CodeGeneration.Domain'],
+    Using: ['System.CodeDom.Compiler', 'Wooli.Foundation.GlassMapper.Models'],
     templatePath: path.join(__dirname, 'codegeneration.tmpl'),
 }

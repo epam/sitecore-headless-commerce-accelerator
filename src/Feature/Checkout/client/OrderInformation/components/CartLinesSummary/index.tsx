fd8212db67d4d4c68f677225e59e8d74aa8ba797
Yuliya Yukhnevich <37637501+yuya-yuliya@users.noreply.github.com>
2020-05-13 17:55:14 +0300

    BUG30 Fix display name of product in Cart Details block on Checkout page (#188)

--- src/Feature/Checkout/client/OrderInformation/components/CartLinesSummary/index.tsx
+++ src/Feature/Checkout/client/OrderInformation/components/CartLinesSummary/index.tsx
@@ -1,11 +1,11 @@
 //    Copyright 2020 EPAM Systems, Inc.
-// 
+//
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
 //    You may obtain a copy of the License at
-// 
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 //    Unless required by applicable law or agreed to in writing, software
 //    distributed under the License is distributed on an "AS IS" BASIS,
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,10 +30,14 @@ export class CartLinesSummary extends Jss.SafePureComponent<CartLinesSummaryProp
           cartLines.map((item, index) => (
             <li key={index}>
               <figure>
-                {item.variant.imageUrls ? (<img src={item.variant.imageUrls[0]} />) : (item.product.imageUrls && (<img src={item.product.imageUrls[0]} />))}
+                {item.variant.imageUrls ? (
+                  <img src={item.variant.imageUrls[0]} />
+                ) : (
+                  item.product.imageUrls && <img src={item.product.imageUrls[0]} />
+                )}
                 <figcaption>
                   <Text field={{ value: item.product.brand }} tag="h4" />
-                  <Text field={{ value: item.product.displayName }} tag="span" />
+                  <Text field={{ value: item.variant.displayName }} tag="span" />
                   <br />
                   <span className="price">
                     {item.price.currencySymbol}

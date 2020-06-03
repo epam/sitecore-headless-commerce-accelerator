ccb913a5f141ef404a0a10b807afc9bf97036371
evgenii-nikulin <61318959+evgenii-nikulin@users.noreply.github.com>
2020-06-03 16:04:17 +0300

    US421-TA422: Separate header rendering (#225)

---
+++ src/Feature/Navigation/client/Header/UserNavigation/models.ts
@@ -0,0 +1,52 @@
+//    Copyright 2020 EPAM Systems, Inc.
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+import * as JSS from 'Foundation/ReactJss/client';
+
+import { RouterProps } from 'react-router';
+
+import { AuthProcessState, GlobalAuthenticationState } from 'Feature/Account/client/Integration/Authentication';
+import { GlobalShoppingCartState } from 'Feature/Checkout/client/Integration/ShoppingCart';
+import * as Commerce from 'Foundation/Commerce/client';
+
+export interface UserNavigationDispatchProps {
+  StartAuth: (email: string, password: string) => void;
+}
+
+export interface UserNavigationOwnProps extends RouterProps {}
+
+export interface UserNavigationStateProps {
+  authProcess: AuthProcessState;
+  cartQuantity: number;
+  commerceUser: Commerce.CommerceUserModel;
+  returnUrl: string;
+}
+
+export interface UserNavigationProps
+  extends JSS.Rendering,
+    UserNavigationOwnProps,
+    UserNavigationStateProps,
+    UserNavigationDispatchProps {}
+
+export interface UserNavigationState extends JSS.SafePureComponentState {
+  userFormVisible: boolean;
+  email: string;
+  formValid: boolean;
+}
+
+export interface AppState
+  extends GlobalAuthenticationState,
+    JSS.RoutingState,
+    GlobalShoppingCartState,
+    JSS.SitecoreState<Commerce.CommerceUserContext> {}

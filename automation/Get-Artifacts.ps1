d620fd3c3066ad8d0ba68c48027c4926d0754cb0
EPMC-STC-CICD <63232604+EPMC-STC-CICD@users.noreply.github.com>
2020-06-08 17:13:59 +0300

    US551 [CICD] Use xml transform in build process (#248)

--- automation/Get-Artifacts.ps1
+++ automation/Get-Artifacts.ps1
@@ -5,7 +5,7 @@ $ArtifactDestinationLayer = Join-Path $ArtifactDestination $ArtifactName
 $SubFoldersList = dir $ArtifactSource | Where-Object {$_.PSIsContainer} | ForEach-Object -Process {$_.FullName}
 
 ForEach ($Folder in $SubFoldersList) {
-	$ArtifactSourcePath = Join-Path $Folder code/obj/Debug/Package/PackageTmp
+	$ArtifactSourcePath = Join-Path $Folder website/obj/Debug/Package/PackageTmp
 	robocopy $ArtifactSourcePath $ArtifactDestinationLayer /s /xo
 	}
 robocopy $ArtifactSource $ArtifactDestination/unicorn/$ArtifactName *.yml /s

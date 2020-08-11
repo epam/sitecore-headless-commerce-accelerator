a5de82bd5f834a02da660af432f1f56e3a5d1f66
An Nguyen <An_Nguyen@epam.com>
2020-08-11 07:58:24 +0000

    US973 [P] Redesign footer and products
    US973 [P] Redesign footer and products

---
+++ src/client/src/Feature/Navigation/FooterLinks2/index.tsx
@@ -0,0 +1,44 @@
+//    Copyright 2020 EPAM Systems, Inc.
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+import { Link } from '@sitecore-jss/sitecore-jss-react';
+import * as React from 'react';
+
+import * as JSS from 'Foundation/ReactJss';
+
+import { FooterLinksProps, FooterLinksState } from './models';
+
+import './styles.scss';
+
+class FooterLinksComponent extends JSS.SafePureComponent<FooterLinksProps, FooterLinksState> {
+  protected safeRender() {
+    const { fields } = this.props;
+    const { datasource } = fields.data;
+
+    return (
+      <ul className="footer-links-list-2">
+        {datasource.links && datasource.links.items && datasource.links.items.map((link, index) => {
+            const { uri, isPrimary } = link;
+            return (
+              <li key={index} className="footer-list-item">
+                <Link field={uri.jss} className={`footer-link footer-link-${isPrimary.jss.value ? 'primary' : 'secondary'}`} />
+              </li>
+            );
+        })}
+      </ul>
+    );
+  }
+}
+
+export const FooterLinks2 = JSS.rendering(FooterLinksComponent);
---
+++ src/client/src/Feature/Navigation/SocialNetworksLinks2/models.ts
@@ -0,0 +1,38 @@
+//    Copyright 2020 EPAM Systems, Inc.
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+import * as Jss from 'Foundation/ReactJss';
+
+export interface SocialNetworkLinkDataSource extends Jss.BaseDataSourceItem {
+    uri: Jss.GraphQLField<Jss.LinkField>;
+}
+
+export interface SocialNetworksLinksDataSource extends Jss.BaseDataSourceItem {
+    sectionTitle: Jss.GraphQLField<Jss.TextField>;
+    links: Jss.GraphQLListField<SocialNetworkLinkDataSource>;
+}
+
+export interface SocialLinksStateProps {
+    isPageEditingMode: boolean;
+}
+
+export interface SocialLinksOwnProps extends Jss.GraphQLRendering<SocialNetworksLinksDataSource> {}
+
+export interface SocialLinksProps extends SocialLinksOwnProps, SocialLinksStateProps {}
+
+export interface SocialLinksState extends Jss.SafePureComponentState {}
+
+export interface AppState extends Jss.SitecoreState<{
+    pageEditing: boolean
+}> {}

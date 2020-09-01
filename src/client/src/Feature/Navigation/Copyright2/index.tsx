ffc5d91224d48b81acedbad74c67ed8f3fec0915
An Nguyen <An_Nguyen@epam.com>
2020-09-01 08:17:08 +0000

    BUG1049 [P] Incorrect footer in redesign UI
    (cherry picked from commit 794c74d9a2ad6521e18bf68a16defcd8a2ff8dbe)

--- src/client/src/Feature/Navigation/Copyright2/index.tsx
+++ src/client/src/Feature/Navigation/Copyright2/index.tsx
@@ -1,11 +1,11 @@
 //    Copyright 2020 EPAM Systems, Inc.
-// 
+//
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
 //    You may obtain a copy of the License at
-// 
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 //    Unless required by applicable law or agreed to in writing, software
 //    distributed under the License is distributed on an "AS IS" BASIS,
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,6 +16,7 @@ import { RichText } from '@sitecore-jss/sitecore-jss-react';
 import * as React from 'react';
 
 import * as JSS from 'Foundation/ReactJss';
+import Logo from 'Foundation/UI/common/media/images/logo-main-black.png';
 
 import { CopyrightProps, CopyrightState } from './models';
 
@@ -28,7 +29,8 @@ class CopyrightComponent extends JSS.SafePureComponent<CopyrightProps, Copyright
 
     return (
       <div className="footer-copyright-container">
-          <RichText className="footer-copyright" field={datasource.text.jss} />
+        <img src={Logo} alt="Discover" className="footer-copyright_logo" />
+        <RichText className="footer-copyright" field={datasource.text.jss} />
       </div>
     );
   }

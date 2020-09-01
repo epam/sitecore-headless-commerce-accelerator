e3477dd2551a7fe9a59a4b4ffdbfba6f90848e06
Keith Eng <Keith_Eng@epam.com>
2020-09-01 10:36:30 +0000

    US1043 Redesign Wishlist

---
+++ src/client/src/Feature/Account/Wishlist2/Component.tsx
@@ -0,0 +1,71 @@
+//    Copyright 2020 EPAM Systems, Inc.
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+import * as JSS from 'Foundation/ReactJss';
+import * as React from 'react';
+
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { NavigationLink } from 'Foundation/UI';
+import { WishlistItem } from './components/WishlistItem';
+
+import { WishlistProps, WishlistState } from './models';
+import './styles.scss';
+
+export class WishlistComponent extends JSS.SafePureComponent<WishlistProps, WishlistState> {
+  public componentDidMount() {
+    this.props.GetWishlist();
+  }
+
+  protected safeRender() {
+    const { title } = this.props.fields.data.datasource;
+    const { items } = this.props;
+    console.log(items);
+    return (
+      <div className="wishlist-container" style={{marginTop: 30}}>
+        <div className="row">
+          <div className="col-xs-12">
+            <div className="wishlist-title">{title.jss.value}</div>
+          </div>
+        </div>
+        {(items.length === 0) ?
+          <div className="wishlist_container-empty" >
+            <FontAwesomeIcon icon={faHeart} size="8x" style={{color: 'black'}} />
+            <label className="wishlist_label-empty">No items found in wishlist</label>
+            <NavigationLink to={`/`}>
+              <button className="wishlist_btn-empty">Add Items</button>
+            </NavigationLink>
+          </div>
+          :
+          <table>
+            <thead className="wishlist_table_header_container">
+              <tr>
+                <th>IMAGE</th>
+                <th>PRODUCT NAME</th>
+                <th>UNIT PRICE</th>
+                <th>ADD TO CART</th>
+                <th>ACTION</th>
+              </tr>
+            </thead>
+            <tbody>
+              {items.map((item, index) => (
+                <WishlistItem item={item} key={index} />
+              ))}
+            </tbody>
+          </table>
+        }
+      </div>
+    );
+  }
+}

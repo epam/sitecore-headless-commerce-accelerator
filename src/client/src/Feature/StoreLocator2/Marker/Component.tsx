1057ae9e9a01e97d24bbdfa90cd70c8b4a429434
Elena Petrashen <Elena_Petrashen@epam.com>
2020-09-22 08:49:06 +0000

    BUG1102 StoreLocator marker message does not collapse

--- src/client/src/Feature/StoreLocator2/Marker/Component.tsx
+++ src/client/src/Feature/StoreLocator2/Marker/Component.tsx
@@ -6,6 +6,9 @@ import { InfoWindow, Marker as GoogleMapMarker } from 'react-google-maps';
 import { MarkerProps, MarkerState } from './models';
 
 export class Marker extends JSS.SafePureComponent<MarkerProps, MarkerState> {
+
+  private divRef = React.createRef<HTMLInputElement>();
+
   public constructor(props: MarkerProps) {
     super(props);
 
@@ -16,6 +19,27 @@ export class Marker extends JSS.SafePureComponent<MarkerProps, MarkerState> {
     this.onToggleOpen = this.onToggleOpen.bind(this);
   }
 
+  public handleClick = (e: MouseEvent) => {
+    const currentRef = this.divRef.current;
+    if (!this.state.isOpen || !currentRef || currentRef.contains(e.target as Element)) {
+      return;
+    }
+
+    this.setState({
+      isOpen: false,
+    });
+  };
+
+  public componentDidMount() {
+    document.addEventListener('mousedown', this.handleClick);
+    document.addEventListener('touchstart', this.handleClick);
+  }
+
+  public componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClick);
+    document.removeEventListener('touchend', this.handleClick);
+  }
+
   protected safeRender() {
     return (
       <GoogleMapMarker
@@ -24,7 +48,7 @@ export class Marker extends JSS.SafePureComponent<MarkerProps, MarkerState> {
       >
         {this.state.isOpen && (
           <InfoWindow onCloseClick={this.onToggleOpen}>
-            <div>
+            <div ref={this.divRef}>
               <h4>{this.props.information.title}</h4>
               <p>{this.props.information.description}</p>
             </div>

57a88686fcaf12e75124a8ecff2ff2493e5b41a0
Danh Nguyen <Danh_Nguyen@epam.com>
2020-09-24 14:44:43 +0000

    US1111 [P] Redesign Cart List Item
    (cherry picked from commit a4e95a29bf3590b6143527bda6e696fffeb36fe9)

---
+++ src/client/src/Feature/Checkout/Cart2/components/CartSummary/models.ts
@@ -0,0 +1,39 @@
+//    Copyright 2020 EPAM Systems, Inc.
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+import * as JSS from 'Foundation/ReactJss';
+
+import { ProductColors, Variant } from 'Foundation/Commerce';
+
+import { ShoppingCart as ShoppingCartApi } from 'Feature/Checkout/Integration/api';
+import * as ShoppingCart from 'Feature/Checkout/Integration/ShoppingCart';
+
+export interface CartSummaryOwnProps {
+  cartLines: ShoppingCart.ShoppingCartLine[];
+  productColors: ProductColors;
+  fallbackImageUrl: string;
+}
+export interface CartSummaryStateProps {
+  isLoading: boolean;
+}
+export interface CartSummaryDispatchProps {
+  AddWishlistItem: (item: Variant) => void;
+  UpdateCartLine: (model: ShoppingCartApi.CartItemDto) => void;
+  RemoveCartLine: (model: ShoppingCart.ShoppingCartLine) => void;
+}
+
+export interface CartSummaryProps extends CartSummaryOwnProps, CartSummaryStateProps, CartSummaryDispatchProps {}
+export interface CartSummaryState extends JSS.SafePureComponentState {}
+
+export interface AppState extends ShoppingCart.GlobalShoppingCartState {}

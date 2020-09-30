e997362c680462916e24f7f40bbfe45707b50589
Mike Maharjan <Mike_Maharjan@epam.com>
2020-09-30 09:59:47 +0000

    BUG621 [P] Payment failure

--- src/client/src/Feature/Checkout/Payment/index.ts
+++ src/client/src/Feature/Checkout/Payment/index.ts
@@ -1,11 +1,11 @@
 //    Copyright 2020 EPAM Systems, Inc.
-// 
+//
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
 //    You may obtain a copy of the License at
-// 
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 //    Unless required by applicable law or agreed to in writing, software
 //    distributed under the License is distributed on an "AS IS" BASIS,
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,7 +27,10 @@ import * as Models from './models';
 const mapStateToProps = (state: Models.AppState) => {
   const currentStep = Checkout.currentStep(state);
 
-  return { isSubmitting: currentStep.status === LoadingStatus.Loading };
+  return {
+    isSubmitting: currentStep.status === LoadingStatus.Loading,
+    paymentStatusFailed: currentStep.status === LoadingStatus.Failure,
+  };
 };
 
 const mapDispatchToProps = (dispatch: Dispatch) =>
@@ -36,12 +39,12 @@ const mapDispatchToProps = (dispatch: Dispatch) =>
       InitStep: Checkout.InitStep,
       SubmitStep: Checkout.SubmitStep,
     },
-    dispatch
+    dispatch,
   );
 
 const connectedToStore = connect<Models.PaymentStateProps, Models.PaymentDispatchProps, Models.PaymentOwnProps>(
   mapStateToProps,
-  mapDispatchToProps
+  mapDispatchToProps,
 );
 
 export const Payment = compose(connectedToStore, renderingWithContext)(PaymentComponent);

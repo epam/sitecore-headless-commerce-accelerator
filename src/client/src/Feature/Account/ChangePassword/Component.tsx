7bf83b3a9ad1663e039dc06311a3b647f394473f
Mike Maharjan <Mike_Maharjan@epam.com>
2020-10-05 18:04:08 +0000

    BUG502 [P] Account page error

--- src/client/src/Feature/Account/ChangePassword/Component.tsx
+++ src/client/src/Feature/Account/ChangePassword/Component.tsx
@@ -1,11 +1,11 @@
 //    Copyright 2020 EPAM Systems, Inc.
-// 
+//
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
 //    You may obtain a copy of the License at
-// 
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 //    Unless required by applicable law or agreed to in writing, software
 //    distributed under the License is distributed on an "AS IS" BASIS,
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,14 +29,16 @@ export default class ChangePasswordComponent extends Jss.SafePureComponent<
   ChangePasswordOwnState
 > {
   public componentDidMount() {
-    if (!this.props.sitecoreContext.pageEditing) {
+    {
+      /* verify the commerce user only in normal mode..  */
+    }
+    if (this.props.sitecoreContext.pageState === 'normal') {
       this.props.VerifyCommerceUser();
     }
   }
 
   protected safeRender() {
     const { changePasswordState } = this.props;
-
     const isLoading = changePasswordState.status === LoadingStatus.Loading;
     const isChanged = changePasswordState.status === LoadingStatus.Loaded;
     const isError = changePasswordState.status === LoadingStatus.Failure;

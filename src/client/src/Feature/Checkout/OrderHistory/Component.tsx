61e5ed028c1e048a91302d7b9070a5d479c564d8
An Nguyen <An_Nguyen@epam.com>
2020-10-23 09:21:36 +0000

    BUG1305 [P] order history no items

--- src/client/src/Feature/Checkout/OrderHistory/Component.tsx
+++ src/client/src/Feature/Checkout/OrderHistory/Component.tsx
@@ -1,11 +1,11 @@
 //    Copyright 2020 EPAM Systems, Inc.
-// 
+//
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
 //    You may obtain a copy of the License at
-// 
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 //    Unless required by applicable law or agreed to in writing, software
 //    distributed under the License is distributed on an "AS IS" BASIS,
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,6 +13,7 @@
 //    limitations under the License.
 
 import { Text } from '@sitecore-jss/sitecore-jss-react';
+import { NavigationLink } from 'Foundation/UI';
 import * as React from 'react';
 
 import * as JSS from 'Foundation/ReactJss';
@@ -35,30 +36,48 @@ export class OrderHistoryComponent extends JSS.SafePureComponent<OrderHistoryPro
     const { orders, isLastPage, isLoading, sitecoreContext } = this.props;
     return (
       <div className="order-history">
-        <div className="color-title">
-          <Text tag="h1" field={{ value: 'Order History' }} className="title" />
-          <div className="color-bar" />
-        </div>
-        <div className="order-list">
-          {orders &&
-            orders.map((order, index) => (
-              <div className="row order-list-item" key={index}>
-                <OrderHistoryItem order={order} fallbackImageUrl={sitecoreContext.fallbackImageUrl} />
+        {orders && orders.length > 0 ? (
+          <div>
+            <div className="color-title">
+              <Text tag="h1" field={{ value: 'Order History' }} className="title" />
+              <div className="color-bar" />
+            </div>
+            <div className="order-list">
+              {orders.map((order, index) => (
+                <div className="row order-list-item" key={index}>
+                  <OrderHistoryItem order={order} fallbackImageUrl={sitecoreContext.fallbackImageUrl} />
+                </div>
+              ))}
+            </div>
+            {isLoading && (
+              <div className="order-history-loader">
+                F
+                <div className="object object-one" />
+                <div className="object object-two" />
+                <div className="object object-three" />
               </div>
-            ))}
-        </div>
-        {isLoading && (
-          <div className="order-history-loader">
-            <div className="object object-one" />
-            <div className="object object-two" />
-            <div className="object object-three" />
+            )}
+            {!isLastPage && !isLoading && (
+              <div className="order-history-loadMore">
+                <a
+                  className="btn btn-outline-main btn-block btn-load-more"
+                  href="#"
+                  onClick={(e) => this.loadMoreHandler(e)}
+                >
+                  Load more
+                </a>
+              </div>
+            )}
           </div>
-        )}
-        {!isLastPage && !isLoading &&  (
-          <div className="order-history-loadMore">
-            <a className="btn btn-outline-main btn-block btn-load-more" href="#" onClick={(e) => this.loadMoreHandler(e)}>
-              Load more
-          </a>
+        ) : (
+          <div className="order-history_container-empty">
+            <div className="order-history-icon">
+              <i className="pe-7s-note2" />
+            </div>
+            <label className="order-history_label-empty">Order history is empty</label>
+            <NavigationLink to={'/'}>
+              <button className="order-history_btn-empty">Shop Now</button>
+            </NavigationLink>
           </div>
         )}
       </div>

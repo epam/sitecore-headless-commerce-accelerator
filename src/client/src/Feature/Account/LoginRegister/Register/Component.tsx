4082e8422f66ca30ba4349c61b81f17114d1dc3a
Keith Eng <Keith_Eng@epam.com>
2020-10-29 10:13:04 +0000

    BUG1277 [P] Register Page Bugs

--- src/client/src/Feature/Account/LoginRegister/Register/Component.tsx
+++ src/client/src/Feature/Account/LoginRegister/Register/Component.tsx
@@ -29,6 +29,13 @@ export class RegisterComponent extends Jss.SafePureComponent<SignUpProps, SignUp
 
     this.state = {
       isSignUp: false,
+
+      isConfirmPasswordEmpty: false,
+      isEmailValid: true,
+      isFirstNameEmpty: false,
+      isLastNameEmpty: false,
+      isPasswordEmpty: false,
+      isPasswordsValid: true,
     };
   }
   public componentWillUnmount() {
@@ -37,62 +44,79 @@ export class RegisterComponent extends Jss.SafePureComponent<SignUpProps, SignUp
 
   // tslint:disable-next-line:cognitive-complexity
   protected safeRender() {
-    const { loading, accountValidation, AccountValidation } = this.props;
-    const submitButtonDisabled =
-      loading || accountValidation.invalid || accountValidation.status === LoadingStatus.Loading;
+    const { loading, accountValidation } = this.props;
+    const { isConfirmPasswordEmpty, isEmailValid, isFirstNameEmpty, isLastNameEmpty, isPasswordEmpty, isPasswordsValid } = this.state;
     return (
       <div className="register_form">
         <Form>
           <div className="form-field">
-            <Input type="text" name={FORM_FIELDS.FIRST_NAME} required={true} placeholder="First Name" />
+            <Input type="text" name={FORM_FIELDS.FIRST_NAME} placeholder="First Name" />
+            {isFirstNameEmpty && (
+              <div className="form-field-error-message">First Name field is required!</div>
+            )}
           </div>
           <div className="form-field">
-            <Input type="text" name={FORM_FIELDS.LAST_NAME} required={true} placeholder="Last Name" />
+            <Input type="text" name={FORM_FIELDS.LAST_NAME} placeholder="Last Name" />
+            {isLastNameEmpty && (
+              <div className="form-field-error-message">Last Name field is required!</div>
+            )}
           </div>
           <div className="form-field">
             <Input
               type="email"
               name={FORM_FIELDS.EMAIL}
-              required={true}
-              onBlur={(e) => {
-                if (e.target.validity.valid) {
-                  AccountValidation(e.target.value);
-                }
-              }}
               placeholder="Email"
             />
-            {accountValidation.status === LoadingStatus.Loading && (
-              <span className="form-field-async-validation">
-                <i className="fa fa-spinner fa-spin" />
-              </span>
+            {(!isEmailValid || accountValidation.inUse || accountValidation.invalid) && (
+              <div className="form-field-error-message">
+                {!isEmailValid
+                  ? 'Email field is required!'
+                  : accountValidation.inUse
+                    ? 'Email is already in use!'
+                    : 'Email is invalid!'
+                }
+              </div>
             )}
-            {accountValidation.status === LoadingStatus.Loaded && (
-              <span className="form-field-error-message">
-                {accountValidation.inUse && 'Email is already in use!'}
-                {accountValidation.invalid && 'Email is invalid!'}
-              </span>
-            )}
+            {/* {accountValidation.status === LoadingStatus.Loaded
+              && ((isEmailValid || accountValidation.inUse || accountValidation.invalid) && (
+              <div className="form-field-error-message">
+                {isEmailValid
+                  ? 'Email field is required!'
+                  : accountValidation.inUse
+                    ? 'Email is already in use!'
+                    : 'Email is invalid!'
+                }
+              </div>
+            ))} */}
           </div>
           <div className="form-field">
             <Input
               type="password"
               name={FORM_FIELDS.PASSWORD}
-              required={true}
-              customValidator={(formValues) => this.passwordCustomValidator(formValues)}
               placeholder="Password"
             />
+            {isPasswordEmpty && (
+              <div className="form-field-error-message">Password field is required!</div>
+            )}
           </div>
           <div className="form-field">
             <Input
               type="password"
               name={FORM_FIELDS.CONFIRM_PASSWORD}
-              required={true}
-              customValidator={(formValues) => this.passwordCustomValidator(formValues)}
               placeholder="Confirm Password"
             />
+            {(isConfirmPasswordEmpty || !isPasswordsValid) && (
+              <div className="form-field-error-message">
+                {
+                  isConfirmPasswordEmpty
+                  ? 'Confirm Password field is required!'
+                  : 'Passwords do not match!'
+                }
+              </div>
+            )}
           </div>
           <Submit
-            disabled={submitButtonDisabled}
+            disabled={false}
             className="btn-login-register"
             onSubmitHandler={(formValues) => this.handleFormSubmit(formValues)}
           >
@@ -106,18 +130,100 @@ export class RegisterComponent extends Jss.SafePureComponent<SignUpProps, SignUp
 
   private handleFormSubmit(formValues: FormValues) {
     const { CreateAccount, returnUrl } = this.props;
+    const { accountValidation } = this.props;
 
-    const createAccountDto = {
-      email: formValues[FORM_FIELDS.EMAIL] as string,
-      firstName: formValues[FORM_FIELDS.FIRST_NAME] as string,
-      lastName: formValues[FORM_FIELDS.LAST_NAME] as string,
-      password: formValues[FORM_FIELDS.PASSWORD] as string,
-    };
+    const isFirstNameValid = this.validateFirstName(formValues);
+    const isLastNameValid = this.validateLastName(formValues);
+    const isEmailValid = this.validateEmail(formValues);
+    const isPasswordValid = this.validatePassword(formValues);
+    const isConfirmPasswordValid = this.validateConfirmPassword(formValues);
+    let isPasswordsMatch = false;
+    if (isPasswordValid && isConfirmPasswordValid) {
+      isPasswordsMatch = this.passwordValidator(formValues);
+    }
 
-    CreateAccount(createAccountDto, returnUrl);
+    if (isFirstNameValid
+      && isLastNameValid
+      && isEmailValid
+      && isPasswordsMatch
+      && (accountValidation.status === LoadingStatus.Loaded
+        && accountValidation.inUse === false
+        && accountValidation.invalid === false)
+      ) {
+        const createAccountDto = {
+          email: formValues[FORM_FIELDS.EMAIL] as string,
+          firstName: formValues[FORM_FIELDS.FIRST_NAME] as string,
+          lastName: formValues[FORM_FIELDS.LAST_NAME] as string,
+          password: formValues[FORM_FIELDS.PASSWORD] as string,
+        };
+        CreateAccount(createAccountDto, returnUrl);
+    }
   }
 
-  private passwordCustomValidator(formValues: FormValues) {
-    return formValues[FORM_FIELDS.PASSWORD] === formValues[FORM_FIELDS.CONFIRM_PASSWORD];
+  private passwordValidator(formValues: FormValues) {
+    if (formValues[FORM_FIELDS.PASSWORD] === formValues[FORM_FIELDS.CONFIRM_PASSWORD]) {
+      this.setState({ isPasswordsValid: true });
+      return true;
+    } else {
+      this.setState({ isPasswordsValid: false });
+      return false;
+    }
+  }
+  private validateFirstName(form: FormValues) {
+    const fName = form[FORM_FIELDS.FIRST_NAME] as string;
+    if (form && fName) {
+      this.setState({ isFirstNameEmpty: false });
+      return true;
+    } else {
+      this.setState({ isFirstNameEmpty: true });
+      return false;
+    }
+  }
+
+  private validateLastName(form: FormValues) {
+    const lName = form[FORM_FIELDS.LAST_NAME] as string;
+    if (form && lName) {
+      this.setState({ isLastNameEmpty: false });
+      return true;
+    } else {
+      this.setState({ isLastNameEmpty: true });
+      return false;
+    }
+  }
+
+  private validateEmail(form: FormValues) {
+    const { AccountValidation } = this.props;
+    const email = form[FORM_FIELDS.EMAIL] as string;
+
+    if (email && (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email))) {
+      AccountValidation(email);
+      this.setState({ isEmailValid: true });
+      return true;
+    } else {
+      this.setState({ isEmailValid: false });
+      return false;
+    }
+  }
+
+  private validatePassword(form: FormValues) {
+    const password = form[FORM_FIELDS.PASSWORD] as string;
+    if (form && password) {
+      this.setState({ isPasswordEmpty: false });
+      return true;
+    } else {
+      this.setState({ isPasswordEmpty: true });
+      return false;
+    }
+  }
+
+  private validateConfirmPassword(form: FormValues) {
+    const confirmPassword = form[FORM_FIELDS.CONFIRM_PASSWORD] as string;
+    if (form && confirmPassword) {
+      this.setState({ isConfirmPasswordEmpty: false });
+      return true;
+    } else {
+      this.setState({ isConfirmPasswordEmpty: true });
+      return false;
+    }
   }
 }

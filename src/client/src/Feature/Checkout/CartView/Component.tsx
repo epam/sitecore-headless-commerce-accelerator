faf71b920a3b705a025685ecec4efcefe899305a
An Nguyen <An_Nguyen@epam.com>
2020-10-30 08:18:32 +0000

    BUG1330 [P] Add initialize loading spinner Order history page

--- src/client/src/Feature/Checkout/CartView/Component.tsx
+++ src/client/src/Feature/Checkout/CartView/Component.tsx
@@ -74,8 +74,12 @@ export default class Cart extends Jss.SafePureComponent<CartProps, CartState> {
           </h4>
         </div>
         <div className="shopping-cart-view-populated-buttons">
-          <NavigationLink to="/cart" className="shopping-cart-view-populated-buttons-view">View Cart</NavigationLink>
-          <NavigationLink to="/checkout/shipping" className="shopping-cart-view-populated-buttons-checkout">Checkout</NavigationLink>
+          <NavigationLink to="/cart" className="shopping-cart-view-populated-buttons-view">
+            View Cart
+          </NavigationLink>
+          <NavigationLink to="/checkout/shipping" className="shopping-cart-view-populated-buttons-checkout">
+            Checkout
+          </NavigationLink>
         </div>
       </div>
     ) : (

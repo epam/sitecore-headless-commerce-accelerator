aea1da36ad1130e505129d194a0ec299f55ad973
An Nguyen <An_Nguyen@epam.com>
2020-11-11 09:08:29 +0000

    BUG1337 [P] fix responsive cart page
    (cherry picked from commit 8b46fe70546019db6eeaec8e4311715b1dc6446c)

--- src/client/src/Feature/Checkout/Cart/components/CartSummary/Component.tsx
+++ src/client/src/Feature/Checkout/Cart/components/CartSummary/Component.tsx
@@ -38,91 +38,93 @@ export class CartSummaryComponent extends Jss.SafePureComponent<CartSummaryProps
             <div className="loading" />
           </div>
         )}
-        <section className="cartSummary" data-autotests="cartSummarySection">
-          <header className="cartSummary-header">
-            <h2 className="col-xs-2 header-title product-image">IMAGE</h2>
-            <h2 className="col-xs-2 header-title product-details">PRODUCT NAME</h2>
-            <h2 className="col-xs-2 header-title product-price">UNIT PRICE</h2>
-            <h2 className="col-xs-2 header-title product-quantity">QTY</h2>
-            <h2 className="col-xs-2 header-title product-subtotal">SUBTOTAL</h2>
-            <h2 className="col-xs-2 header-title product-actions">ACTION</h2>
-          </header>
-          <ul className="cartList">
-            {cartLines.map((cartLine) => {
-              let imageUrl = fallbackImageUrl;
-              const isUpdatedPrice = cartLine.variant.listPrice !== cartLine.variant.adjustedPrice;
-              if (!!cartLine.variant.imageUrls && cartLine.variant.imageUrls.length > 0) {
-                imageUrl = cartLine.variant.imageUrls[0];
-              } else if (!!cartLine.product.imageUrls && cartLine.product.imageUrls.length > 0) {
-                imageUrl = cartLine.product.imageUrls[0];
-              }
 
-              return (
-                <li className="cartList-product" key={cartLine.id} data-autotests={`cartProduct_${cartLine.id}`}>
-                  <div className="product">
-                    <div className="col-xs-2 product-image ">
-                      <img src={imageUrl} />
-                    </div>
-                    <div className="col-xs-2 product-details">
-                      <div className="product-info">
-                        <div className="product-heading">
-                          <NavigationLink
-                            className="heading-productTitle"
-                            to={`/product/${cartLine.product.productId}`}
-                            data-autotests="productInfoTitle"
-                          >
-                            {cartLine.variant.displayName || cartLine.product.displayName}
-                          </NavigationLink>
-                        </div>
-                        <div className="product-options" data-autotests="productOptions">
-                          {cartLine.variant.properties.color && (
-                            <div className="option">
-                              <label>Color:</label>
-                              <span>{cartLine.variant.properties.color}</span>
-                            </div>
-                          )}
-                          {cartLine.variant.properties.size && (
-                            <div className="option">
-                              Size:<span className="size">{cartLine.variant.properties.size}</span>
-                            </div>
-                          )}
+        <div className="cart-summary-container">
+          <section className="cartSummary" data-autotests="cartSummarySection">
+            <header className="cartSummary-header">
+              <h2 className="col-xs-2 header-title product-image">IMAGE</h2>
+              <h2 className="col-xs-2 header-title product-details">PRODUCT NAME</h2>
+              <h2 className="col-xs-2 header-title product-price">UNIT PRICE</h2>
+              <h2 className="col-xs-2 header-title product-quantity">QTY</h2>
+              <h2 className="col-xs-2 header-title product-subtotal">SUBTOTAL</h2>
+              <h2 className="col-xs-2 header-title product-actions">ACTION</h2>
+            </header>
+            <ul className="cartList">
+              {cartLines.map((cartLine) => {
+                let imageUrl = fallbackImageUrl;
+                const isUpdatedPrice = cartLine.variant.listPrice > cartLine.variant.adjustedPrice;
+                if (!!cartLine.variant.imageUrls && cartLine.variant.imageUrls.length > 0) {
+                  imageUrl = cartLine.variant.imageUrls[0];
+                } else if (!!cartLine.product.imageUrls && cartLine.product.imageUrls.length > 0) {
+                  imageUrl = cartLine.product.imageUrls[0];
+                }
+
+                return (
+                  <li className="cartList-product" key={cartLine.id} data-autotests={`cartProduct_${cartLine.id}`}>
+                    <div className="product">
+                      <div className="col-xs-2 product-image ">
+                        <img src={imageUrl} />
+                      </div>
+                      <div className="col-xs-2 product-details">
+                        <div className="product-info">
+                          <div className="product-heading">
+                            <NavigationLink
+                              className="heading-productTitle"
+                              to={`/product/${cartLine.product.productId}`}
+                              data-autotests="productInfoTitle"
+                            >
+                              {cartLine.variant.displayName || cartLine.product.displayName}
+                            </NavigationLink>
+                          </div>
+                          <div className="product-options" data-autotests="productOptions">
+                            {cartLine.variant.properties.color && (
+                              <div className="option">
+                                <label>Color:</label>
+                                <span>{cartLine.variant.properties.color}</span>
+                              </div>
+                            )}
+                            {cartLine.variant.properties.size && (
+                              <div className="option">
+                                Size:<span className="size">{cartLine.variant.properties.size}</span>
+                              </div>
+                            )}
+                          </div>
                         </div>
                       </div>
-                    </div>
-                    <div className="col-xs-2 amount product-price">
-
-                      <div className={isUpdatedPrice ? 'discount' : 'origin'}>
-                        <span className="currency">{cartLine.variant.currencySymbol}</span>
-                        {cartLine.variant.listPrice.toFixed(2)}
-                      </div>
-                      {isUpdatedPrice &&
-                        <div>
+                      <div className="col-xs-2 amount product-price">
+                        <div className={isUpdatedPrice ? 'discount' : 'origin'}>
                           <span className="currency">{cartLine.variant.currencySymbol}</span>
-                          {cartLine.variant.adjustedPrice.toFixed(2)}
+                          {cartLine.variant.listPrice.toFixed(2)}
                         </div>
-                      }
+                        {isUpdatedPrice && (
+                          <div>
+                            <span className="currency">{cartLine.variant.currencySymbol}</span>
+                            {cartLine.variant.adjustedPrice.toFixed(2)}
+                          </div>
+                        )}
+                      </div>
+                      <div className="col-xs-2 amount quantity-group product-quantity">
+                        <Quantity cartLine={cartLine} UpdateCartLine={UpdateCartLine} RemoveCartLine={RemoveCartLine} />
+                      </div>
+                      <div className="col-xs-2 amount product-subtotal">
+                        <span className="currency">{cartLine.price.currencySymbol}</span>
+                        {cartLine.price.total.toFixed(2)}
+                      </div>
+                      <div className="col-xs-2 actions product-actions">
+                        <button>
+                          <i className="fa fa-heart" />
+                        </button>
+                        <button>
+                          <i className="fa fa-times" onClick={(e) => RemoveCartLine(cartLine)} />
+                        </button>
+                      </div>
                     </div>
-                    <div className="col-xs-2 amount quantity-group product-quantity">
-                      <Quantity cartLine={cartLine} UpdateCartLine={UpdateCartLine} RemoveCartLine={RemoveCartLine} />
-                    </div>
-                    <div className="col-xs-2 amount product-subtotal">
-                      <span className="currency">{cartLine.price.currencySymbol}</span>
-                      {cartLine.price.total.toFixed(2)}
-                    </div>
-                    <div className="col-xs-2 actions product-actions">
-                      <button>
-                        <i className="fa fa-heart fa-lg" />
-                      </button>
-                      <button>
-                        <i className="fa fa-times" onClick={(e) => RemoveCartLine(cartLine)} />
-                      </button>
-                    </div>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
-        </section>
+                  </li>
+                );
+              })}
+            </ul>
+          </section>
+        </div>
       </>
     );
   }

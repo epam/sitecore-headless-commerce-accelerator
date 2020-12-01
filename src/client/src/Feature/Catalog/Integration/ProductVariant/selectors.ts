58f227db0f19a2ce1a3bac38331d71f8f448124b
Mykola Hutorov <Mykola_Hutorov@epam.com>
2020-12-01 15:50:14 +0000

    BUG1374 [P] Add checks to prevent errors
    (cherry picked from commit 73c3973fcf608eee68350d16799983be1e38132b)

--- src/client/src/Feature/Catalog/Integration/ProductVariant/selectors.ts
+++ src/client/src/Feature/Catalog/Integration/ProductVariant/selectors.ts
@@ -1,11 +1,11 @@
 //    Copyright 2020 EPAM Systems, Inc.
-// 
+//
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
 //    You may obtain a copy of the License at
-// 
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 //    Unless required by applicable law or agreed to in writing, software
 //    distributed under the License is distributed on an "AS IS" BASIS,
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,4 +27,4 @@ export const productId = (state: ProductVariantGlobalState) => {
   const productInStore = product(state);
   return productInStore ? productInStore.productId : '';
 };
-export const variants = (state: ProductVariantGlobalState) => product(state).variants;
+export const variants = (state: ProductVariantGlobalState) => product(state) && product(state).variants;
--- src/client/src/Feature/Catalog/ProductInformation/index.tsx
+++ src/client/src/Feature/Catalog/ProductInformation/index.tsx
@@ -35,158 +35,160 @@ class ProductInformationComponent extends JSS.SafePureComponent<ProductInformati
     const { product } = this.props.sitecoreContext;
     const { selectedTab } = this.state;
     return (
-      <section className="product-description">
-        <div className="tab">
-          <button
-            className={`tab-links ${selectedTab === 'features' ? 'active' : ''}`}
-            onClick={() => this.setState({ selectedTab: 'features' })}
-          >
-            Features
-          </button>
-          <button
-            className={`tab-links ${selectedTab === 'description' ? 'active' : ''}`}
-            onClick={() => this.setState({ selectedTab: 'description' })}
-          >
-            Description
-          </button>
-          <button
-            className={`tab-links ${selectedTab === 'rating' ? 'active' : ''}`}
-            onClick={() => this.setState({ selectedTab: 'rating' })}
-          >
-            Reviews
-          </button>
-        </div>
-        <div className="product-content">
-          {selectedTab === 'description' && (
-            <div className="description-text">
-              <p>{product && product.description}</p>
-            </div>
-          )}
-          {selectedTab === 'features' && (
-            <div className="description-features">
-              <ul className="feature-list">
-                <li>
-                  <span>Data One</span> 2 GB
-                </li>
-                <li>
-                  <span>Data Two</span> Model XE-C
-                </li>
-                <li>
-                  <span>Data Three</span> Charcoal
-                </li>
-                <li>
-                  <span>Data Four</span> Explorer Edition
-                </li>
-              </ul>
-            </div>
-          )}
-          {selectedTab === 'rating' && (
-            <div className="row">
-              <div className="col-lg-7">
-                <div className="review-wrapper">
-                  <div className="single-review">
-                    <div className="review-img">
-                      <img src={pictureOne} alt="" />
-                    </div>
-                    <div className="review-content">
-                      <div className="review-top-wrap">
-                        <div className="review-left">
-                          <div className="review-name">
-                            <h4>White Lewis</h4>
+      product && (
+        <section className="product-description">
+          <div className="tab">
+            <button
+              className={`tab-links ${selectedTab === 'features' ? 'active' : ''}`}
+              onClick={() => this.setState({ selectedTab: 'features' })}
+            >
+              Features
+            </button>
+            <button
+              className={`tab-links ${selectedTab === 'description' ? 'active' : ''}`}
+              onClick={() => this.setState({ selectedTab: 'description' })}
+            >
+              Description
+            </button>
+            <button
+              className={`tab-links ${selectedTab === 'rating' ? 'active' : ''}`}
+              onClick={() => this.setState({ selectedTab: 'rating' })}
+            >
+              Reviews
+            </button>
+          </div>
+          <div className="product-content">
+            {selectedTab === 'description' && (
+              <div className="description-text">
+                <p>{product.description}</p>
+              </div>
+            )}
+            {selectedTab === 'features' && (
+              <div className="description-features">
+                <ul className="feature-list">
+                  <li>
+                    <span>Data One</span> 2 GB
+                  </li>
+                  <li>
+                    <span>Data Two</span> Model XE-C
+                  </li>
+                  <li>
+                    <span>Data Three</span> Charcoal
+                  </li>
+                  <li>
+                    <span>Data Four</span> Explorer Edition
+                  </li>
+                </ul>
+              </div>
+            )}
+            {selectedTab === 'rating' && (
+              <div className="row">
+                <div className="col-lg-7">
+                  <div className="review-wrapper">
+                    <div className="single-review">
+                      <div className="review-img">
+                        <img src={pictureOne} alt="" />
+                      </div>
+                      <div className="review-content">
+                        <div className="review-top-wrap">
+                          <div className="review-left">
+                            <div className="review-name">
+                              <h4>White Lewis</h4>
+                            </div>
+                            <div className="review-rating">
+                              <i className="fa fa-star" />
+                              <i className="fa fa-star" />
+                              <i className="fa fa-star" />
+                              <i className="fa fa-star" />
+                              <i className="fa fa-star" />
+                            </div>
                           </div>
-                          <div className="review-rating">
-                            <i className="fa fa-star" />
-                            <i className="fa fa-star" />
-                            <i className="fa fa-star" />
-                            <i className="fa fa-star" />
-                            <i className="fa fa-star" />
+                          <div className="review-left">
+                            <button>Reply</button>
                           </div>
                         </div>
-                        <div className="review-left">
-                          <button>Reply</button>
+                        <div className="review-bottom">
+                          <p>
+                            Vestibulum ante ipsum primis aucibus orci luctustrices posuere cubilia Curae Suspendisse
+                            viverra ed viverra. Mauris ullarper euismod vehicula. Phasellus quam nisi, congue id nulla.
+                          </p>
                         </div>
                       </div>
-                      <div className="review-bottom">
-                        <p>
-                          Vestibulum ante ipsum primis aucibus orci luctustrices posuere cubilia Curae Suspendisse
-                          viverra ed viverra. Mauris ullarper euismod vehicula. Phasellus quam nisi, congue id nulla.
-                        </p>
+                    </div>
+                    <div className="single-review child-review">
+                      <div className="review-img">
+                        <img src={pictureTwo} alt="" />
                       </div>
-                    </div>
-                  </div>
-                  <div className="single-review child-review">
-                    <div className="review-img">
-                      <img src={pictureTwo} alt="" />
-                    </div>
-                    <div className="review-content">
-                      <div className="review-top-wrap">
-                        <div className="review-left">
-                          <div className="review-name">
-                            <h4>White Lewis</h4>
+                      <div className="review-content">
+                        <div className="review-top-wrap">
+                          <div className="review-left">
+                            <div className="review-name">
+                              <h4>White Lewis</h4>
+                            </div>
+                            <div className="review-rating">
+                              <i className="fa fa-star" />
+                              <i className="fa fa-star" />
+                              <i className="fa fa-star" />
+                              <i className="fa fa-star" />
+                              <i className="fa fa-star" />
+                            </div>
                           </div>
-                          <div className="review-rating">
-                            <i className="fa fa-star" />
-                            <i className="fa fa-star" />
-                            <i className="fa fa-star" />
-                            <i className="fa fa-star" />
-                            <i className="fa fa-star" />
+                          <div className="review-left">
+                            <button>Reply</button>
                           </div>
                         </div>
-                        <div className="review-left">
-                          <button>Reply</button>
+                        <div className="review-bottom">
+                          <p>
+                            Vestibulum ante ipsum primis aucibus orci luctustrices posuere cubilia Curae Suspendisse
+                            viverra ed viverra. Mauris ullarper euismod vehicula. Phasellus quam nisi, congue id nulla.
+                          </p>
                         </div>
-                      </div>
-                      <div className="review-bottom">
-                        <p>
-                          Vestibulum ante ipsum primis aucibus orci luctustrices posuere cubilia Curae Suspendisse
-                          viverra ed viverra. Mauris ullarper euismod vehicula. Phasellus quam nisi, congue id nulla.
-                        </p>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <div className="col-lg-5">
-                <div className="ratting-form-wrapper pl-50">
-                  <h3>Add a Review</h3>
-                  <div className="ratting-form">
-                    <form action="#">
-                      <div className="star-box">
-                        <span>Your rating:</span>
-                        <div className="ratting-star">
-                          <i className="fa fa-star" />
-                          <i className="fa fa-star" />
-                          <i className="fa fa-star" />
-                          <i className="fa fa-star" />
-                          <i className="fa fa-star" />
-                        </div>
-                      </div>
-                      <div className="row">
-                        <div className="col-md-6">
-                          <div className="rating-form-style mb-10">
-                            <input placeholder="Name" type="text" />
+                <div className="col-lg-5">
+                  <div className="ratting-form-wrapper pl-50">
+                    <h3>Add a Review</h3>
+                    <div className="ratting-form">
+                      <form action="#">
+                        <div className="star-box">
+                          <span>Your rating:</span>
+                          <div className="ratting-star">
+                            <i className="fa fa-star" />
+                            <i className="fa fa-star" />
+                            <i className="fa fa-star" />
+                            <i className="fa fa-star" />
+                            <i className="fa fa-star" />
                           </div>
                         </div>
-                        <div className="col-md-6">
-                          <div className="rating-form-style mb-10">
-                            <input placeholder="Email" type="email" />
+                        <div className="row">
+                          <div className="col-md-6">
+                            <div className="rating-form-style mb-10">
+                              <input placeholder="Name" type="text" />
+                            </div>
+                          </div>
+                          <div className="col-md-6">
+                            <div className="rating-form-style mb-10">
+                              <input placeholder="Email" type="email" />
+                            </div>
+                          </div>
+                          <div className="col-md-12">
+                            <div className="rating-form-style form-submit">
+                              <textarea name="Your Review" placeholder="Message" defaultValue={''} />
+                              <input type="submit" defaultValue="Submit" />
+                            </div>
                           </div>
                         </div>
-                        <div className="col-md-12">
-                          <div className="rating-form-style form-submit">
-                            <textarea name="Your Review" placeholder="Message" defaultValue={''} />
-                            <input type="submit" defaultValue="Submit" />
-                          </div>
-                        </div>
-                      </div>
-                    </form>
+                      </form>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          )}
-        </div>
-      </section>
+            )}
+          </div>
+        </section>
+      )
     );
   }
 }

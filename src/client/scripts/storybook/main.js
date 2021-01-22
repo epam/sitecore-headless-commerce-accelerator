60d709a60de0c54df2df26b7dd400ed610f6f78e
Andrey Lavrov <Andrey_Lavrov@epam.com>
2021-01-22 09:59:36 +0000

    US1419 [P] RadioButton global component
    (cherry picked from commit 762c789ab14cf3974980d3b91d85114f1e5e14c5)

---
+++ src/client/scripts/storybook/main.js
@@ -0,0 +1,44 @@
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  stories: ['../../src/**/stories/**/*.stories.mdx', '../../src/**/stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  webpackFinal: (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        {
+          loader: 'style-loader',
+        },
+        {
+          loader: 'css-loader',
+        },
+        {
+          loader: require.resolve('postcss-loader'),
+          options: {
+            // Necessary for external CSS imports to work
+            // https://github.com/facebookincubator/create-react-app/issues/2677
+            ident: 'postcss',
+            plugins: () => [
+              require('postcss-flexbugs-fixes'),
+              require('postcss-object-fit-images'),
+              autoprefixer({
+                browsers: [
+                  '>1%',
+                  'last 4 versions',
+                  'Firefox ESR',
+                  'not ie < 9', // React doesn't support IE8 anyway
+                ],
+                flexbox: 'no-2009',
+              }),
+            ],
+          },
+        },
+        {
+          loader: require.resolve('sass-loader'),
+        },
+      ],
+    });
+
+    return config;
+  },
+};

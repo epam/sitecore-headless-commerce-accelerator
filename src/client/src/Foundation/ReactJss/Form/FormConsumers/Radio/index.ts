c2191bba2659938989d09c36b736d8132c7f2569
Nadezhda Anakina <Nadezhda_Anakina@epam.com>
2021-02-01 12:04:28 +0000

    BUG1540 [P] RadioButton additional props - fixed
    (cherry picked from commit 7aed75a3aadd8f4dc8a5773dcc3caf019a7b30fc)

--- src/client/src/Foundation/ReactJss/Form/FormConsumers/Radio/index.ts
+++ src/client/src/Foundation/ReactJss/Form/FormConsumers/Radio/index.ts
@@ -1,21 +1,24 @@
 //    Copyright 2020 EPAM Systems, Inc.
-// 
+//
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
 //    You may obtain a copy of the License at
-// 
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 //    Unless required by applicable law or agreed to in writing, software
 //    distributed under the License is distributed on an "AS IS" BASIS,
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
+import { RadioButtonProps } from 'Foundation/UI/components/RadioButton';
+
 import { contextConnect } from './../enhancers';
 import { BaseFieldEnhancedProps, BaseFieldProps } from './../models';
 import RadioField from './Radio';
 
-export const Radio = contextConnect<BaseFieldProps<HTMLInputElement>, BaseFieldEnhancedProps<HTMLInputElement>>(
-  RadioField
-);
+export const Radio = contextConnect<
+  BaseFieldProps<HTMLInputElement> & RadioButtonProps,
+  BaseFieldEnhancedProps<HTMLInputElement>
+>(RadioField);

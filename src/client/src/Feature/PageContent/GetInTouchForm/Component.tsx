a84fc9521ddcece9da84546969225f41d41a3375
Elina Samoilenko <Elina_Samoilenko@epam.com>
2021-02-20 14:13:29 +0000

    BUG1481 [P] Fix get in touch form
    (cherry picked from commit fea8507c780d2cf194375596788383dbdcdd772b)

--- src/client/src/Feature/PageContent/GetInTouchForm/Component.tsx
+++ src/client/src/Feature/PageContent/GetInTouchForm/Component.tsx
@@ -13,98 +13,105 @@
 //    limitations under the License.
 
 import * as JSS from 'Foundation/ReactJss';
-import React, { ChangeEvent } from 'react';
+import React, { useState } from 'react';
 
 import { Button } from 'Foundation/UI/components/Button';
 import { validateEmail } from 'Foundation/utils/validation';
 
-import { GetInTouchFormProps, GetInTouchFormState } from './models';
+import { GetInTouchFormProps } from './models';
 import './styles.scss';
 
-export class GetInTouchFormComponent extends JSS.SafePureComponent<GetInTouchFormProps, GetInTouchFormState> {
-  public constructor(props: GetInTouchFormProps) {
-    super(props);
+export const GetInTouchFormComponent: React.FC<GetInTouchFormProps> = (props) => {
+  const [values, setValues] = useState({ email: '', message: '', name: '', subject: '' });
+  const [showStatusMessage, setShowStatusMessage] = useState(false);
 
-    this.state = {
-      email: '',
-      formSubmission: false,
-      message: '',
-      name: '',
-      subject: '',
-    };
-  }
+  const { datasource } = props.fields.data;
 
-  public handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const name = e.currentTarget.name;
-    this.setState({ ...this.state, [name]: e.currentTarget.value.trim(), formSubmission: false });
+  const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setShowStatusMessage(false);
+    setValues({ ...values, [target.name]: target.value });
   };
 
-  protected safeRender() {
-    const { datasource } = this.props.fields.data;
-    const { email, formSubmission, message, name, subject } = this.state;
-    const areFieldsFilled = message && email && name && subject && validateEmail(email);
-    return (
-      <div className="contact-area_content_form">
-        <div className="contact-area_content_form_title">
-          <JSS.Text tag="h2" field={datasource.formTitle.jss} />
-        </div>
-        <form className="contact-area_content_form_style">
-          <div className="row">
-            <div className="col-lg-6">
-              <input
-                name="name"
-                placeholder={datasource.namePlaceholder.jss.value}
-                type="text"
-                value={name}
-                onChange={this.handleChange}
-              />
-            </div>
-            <div className="col-lg-6">
-              <input
-                name="email"
-                placeholder={datasource.emailPlaceholder.jss.value}
-                type="text"
-                value={email}
-                onChange={this.handleChange}
-              />
-            </div>
-            <div className="col-lg-12">
-              <input
-                name="subject"
-                placeholder={datasource.subjectPlaceholder.jss.value}
-                type="text"
-                value={subject}
-                onChange={this.handleChange}
-              />
-            </div>
-            <div className="col-lg-12">
-              <textarea
-                name="message"
-                placeholder={datasource.subjectPlaceholder.jss.value}
-                defaultValue={''}
-                value={message}
-                onChange={this.handleChange}
-              />
-              <div className="Button-Container">
-                <Button buttonType="submit" disabled={!areFieldsFilled} onClick={this.submitContactForm}>
-                  <JSS.Text field={datasource.submitButtonText.jss} />
-                </Button>
-                <span className="Status_Message">
-                  {formSubmission && 'Message is successfully sent! We will be in touch'}
-                </span>
-              </div>
+  const handleBlur = ({ target }: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setValues({ ...values, [target.name]: target.value.trim() });
+  };
+
+  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement>) => {
+    e.preventDefault();
+    setValues({ name: '', email: '', subject: '', message: '' });
+
+    setTimeout(() => setShowStatusMessage(true), 1000);
+  };
+
+  return (
+    <div className="contact-area_content_form">
+      <div className="contact-area_content_form_title">
+        <JSS.Text tag="h2" field={datasource.formTitle.jss} />
+      </div>
+      <form className="contact-area_content_form_style">
+        <div className="row">
+          <div className="col-lg-6">
+            <input
+              name="name"
+              placeholder={datasource.namePlaceholder.jss.value}
+              type="text"
+              value={values.name}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </div>
+          <div className="col-lg-6">
+            <input
+              name="email"
+              placeholder={datasource.emailPlaceholder.jss.value}
+              type="text"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </div>
+          <div className="col-lg-12">
+            <input
+              name="subject"
+              placeholder={datasource.subjectPlaceholder.jss.value}
+              type="text"
+              value={values.subject}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </div>
+          <div className="col-lg-12">
+            <textarea
+              name="message"
+              placeholder={datasource.subjectPlaceholder.jss.value}
+              defaultValue={''}
+              value={values.message}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            <div className="Button-Container">
+              <Button
+                buttonType="submit"
+                disabled={
+                  !(
+                    values.email.trim() &&
+                    values.message.trim() &&
+                    values.name.trim() &&
+                    values.subject.trim() &&
+                    validateEmail(values.email)
+                  )
+                }
+                onClick={(e: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement>) => handleSubmit(e)}
+              >
+                <JSS.Text field={datasource.submitButtonText.jss} />
+              </Button>
+              <span className="Status_Message">
+                {showStatusMessage && 'Message is successfully sent! We will be in touch'}
+              </span>
             </div>
           </div>
-        </form>
-        <p className="form-message" />
-      </div>
-    );
-  }
-
-  private submitContactForm = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    this.setState({ name: '', email: '', subject: '', message: '' });
-
-    setTimeout(() => this.setState({ formSubmission: true }), 1000);
-  };
-}
+        </div>
+      </form>
+    </div>
+  );
+};

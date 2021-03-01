eb36e281106d4f972e13ce78303cf1fe3eb82adf
Andrey Lavrov <Andrey_Lavrov@epam.com>
2021-03-01 13:38:43 +0000

    US1390 [P] Add reusable global components
    (cherry picked from commit 68ca4da2292390d8e6d1e0a9570dea523654fbb8)

---
+++ src/client/src/Foundation/UI/components/Pagination/Pagination.tsx
@@ -0,0 +1,54 @@
+//    Copyright 2020 EPAM Systems, Inc.
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+import React, { FC } from 'react';
+
+import ReactPaginate, { ReactPaginateProps } from 'react-paginate';
+
+import { cnPagination } from './cn';
+
+import './Pagination.scss';
+
+const previousLabel = <i className={'fa fa-angle-double-left'} />;
+const nextLabel = <i className={'fa fa-angle-double-right'} />;
+
+export const Pagination: FC<ReactPaginateProps> = ({
+  pageCount,
+  forcePage,
+  pageRangeDisplayed,
+  marginPagesDisplayed,
+  onPageChange,
+}) => {
+  return (
+    <ReactPaginate
+      containerClassName={cnPagination()}
+      forcePage={forcePage}
+      pageCount={pageCount}
+      pageRangeDisplayed={pageRangeDisplayed}
+      marginPagesDisplayed={marginPagesDisplayed}
+      nextLabel={nextLabel}
+      previousLabel={previousLabel}
+      pageClassName={cnPagination('Item')}
+      previousClassName={cnPagination('Item', { previous: true })}
+      nextClassName={cnPagination('Item', { next: true })}
+      breakClassName={cnPagination('Item', { break: true })}
+      pageLinkClassName={cnPagination('Link')}
+      breakLinkClassName={cnPagination('Link')}
+      previousLinkClassName={cnPagination('Link')}
+      nextLinkClassName={cnPagination('Link')}
+      activeClassName={cnPagination('Item', { active: true })}
+      onPageChange={onPageChange}
+    />
+  );
+};

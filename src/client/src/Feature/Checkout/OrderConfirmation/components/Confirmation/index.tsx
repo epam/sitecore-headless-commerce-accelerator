96905612ddaef029a6cba3aff910c80978f48bdd
Andrey Lavrov <Andrey_Lavrov@epam.com>
2021-04-01 11:48:00 +0000

    US1924 [P] Improve checkout responsiveness
    (cherry picked from commit 41742fad77f17a9f33806bfa7969de4ad97c921c)

--- src/client/src/Feature/Checkout/OrderConfirmation/components/Confirmation/index.tsx
+++ src/client/src/Feature/Checkout/OrderConfirmation/components/Confirmation/index.tsx
@@ -26,124 +26,122 @@ export class Confirmation extends Jss.SafePureComponent<ConfirmationProps, Confi
     const { addresses, payment, price, shipping } = this.props.order;
     return (
       <div className="order-confirmation-wrapper">
-        <section className="container">
-          <div className="row">
-            <div className="col-md-12 nopadding">
-              <div className="order-confirmation">
-                <div className="container">
-                  <div className="row">
-                    <div className="col-md-12">
-                      <Text tag="h3" className="order-confirmation_title" field={{ value: 'Order Confirmation' }} />
-                    </div>
+        <div className="row">
+          <div className="col-md-12 nopadding">
+            <div className="order-confirmation">
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-12">
+                    <Text tag="h3" className="order-confirmation_title" field={{ value: 'Order Confirmation' }} />
                   </div>
                 </div>
-                <div className="order-confirmation_container">
-                  <div className="col-md-4 col-xs-12">
-                    <div className="info-column-1 order-form">
-                      {shipping.map((shippingMethod, i) => {
-                        const address = addresses.find((item) => item.partyId === shippingMethod.partyId);
-                        return (
-                          <div className="shipping-info" key={`${shippingMethod.externalId}-${i}`}>
-                            <div className="info1">
-                              <div className="title">
-                                <Text field={{ value: 'Shipping To' }} tag="span" className="title_text" />
-                              </div>
-                              <Text field={{ value: 'Address' }} tag="div" className="sub-title" />
-                              <div className="sub-content">
-                                <span>
-                                  {address.firstName} {address.lastName}
-                                </span>
-                                <span>{address.address1}</span>
-                                <span>
-                                  {address.city}, {address.state} {address.zipPostalCode}
-                                </span>
-                              </div>
+              </div>
+              <div className="order-confirmation_container">
+                <div className="col-md-4 col-xs-12">
+                  <div className="info-column-1 order-form">
+                    {shipping.map((shippingMethod, i) => {
+                      const address = addresses.find((item) => item.partyId === shippingMethod.partyId);
+                      return (
+                        <div className="shipping-info" key={`${shippingMethod.externalId}-${i}`}>
+                          <div className="info1">
+                            <div className="title">
+                              <Text field={{ value: 'Shipping To' }} tag="span" className="title_text" />
                             </div>
-                            <div className="info2">
-                              <div className="sub-title">Method </div>
-                              <div className="sub-content">{shippingMethod.name}</div>
-                              <div className="sub-title">Status</div>
-                              <div className="sub-content">Pending</div>
+                            <Text field={{ value: 'Address' }} tag="div" className="sub-title" />
+                            <div className="sub-content">
+                              <span>
+                                {address.firstName} {address.lastName}
+                              </span>
+                              <span>{address.address1}</span>
+                              <span>
+                                {address.city}, {address.state} {address.zipPostalCode}
+                              </span>
                             </div>
                           </div>
-                        );
-                      })}
-                    </div>
-                  </div>
-                  <div className="col-md-4 col-xs-12">
-                    <div className="info-column-2 order-form">
-                      {payment.map((paymentInfo, i) => {
-                        const address = addresses.find((item) => item.partyId === paymentInfo.partyId);
-                        return (
-                          <div className="billing-info" key={`${paymentInfo.paymentMethodId}-${i}`}>
-                            <div className="info1">
-                              <div className="title">
-                                <Text field={{ value: 'Billing Information' }} tag="span" className="title_text" />
-                              </div>
-                              <Text field={{ value: 'Address' }} tag="div" className="sub-title" />
-                              <div className="sub-content">
-                                <span>
-                                  {address.firstName} {address.lastName}
-                                </span>
-                                <span>{address.address1}</span>
-                                <span>
-                                  {address.city}, {address.state} {address.zipPostalCode}
-                                </span>
-                              </div>
-                            </div>
+                          <div className="info2">
+                            <div className="sub-title">Method </div>
+                            <div className="sub-content">{shippingMethod.name}</div>
+                            <div className="sub-title">Status</div>
+                            <div className="sub-content">Pending</div>
                           </div>
-                        );
-                      })}
-                    </div>
-                  </div>
-                  <div className="col-md-4 col-xs-12">
-                    <div className="info-column-3 order-form">
-                      <div className="payment-information">
-                        <div className="title">
-                          <Text field={{ value: 'Payment Information:' }} tag="span" className="title_text" />
                         </div>
-                        <div className="payment-information-price">
-                          <div>
-                            <div className="price-item">
-                              <span>Subtotal</span>
+                      );
+                    })}
+                  </div>
+                </div>
+                <div className="col-md-4 col-xs-12">
+                  <div className="info-column-2 order-form">
+                    {payment.map((paymentInfo, i) => {
+                      const address = addresses.find((item) => item.partyId === paymentInfo.partyId);
+                      return (
+                        <div className="billing-info" key={`${paymentInfo.paymentMethodId}-${i}`}>
+                          <div className="info1">
+                            <div className="title">
+                              <Text field={{ value: 'Billing Information' }} tag="span" className="title_text" />
+                            </div>
+                            <Text field={{ value: 'Address' }} tag="div" className="sub-title" />
+                            <div className="sub-content">
                               <span>
-                                {price.currencySymbol}
-                                {price.subtotal.toFixed(2)}
+                                {address.firstName} {address.lastName}
+                              </span>
+                              <span>{address.address1}</span>
+                              <span>
+                                {address.city}, {address.state} {address.zipPostalCode}
                               </span>
                             </div>
-                            <div className="price-item">
-                              <span>Shipping Fees</span>
-                              <span>
-                                {price.currencySymbol}
-                                {price.shippingTotal.toFixed(2)}
-                              </span>
-                            </div>
-                            <div className="price-item">
-                              <span>Taxes</span>
-                              <span>
-                                {price.currencySymbol}
-                                {price.taxTotal.toFixed(2)}
-                              </span>
-                            </div>
-                            <div className="price-item">
-                              <span>Savings</span>
-                              <span>
-                                -{price.currencySymbol}
-                                {price.totalSavings.toFixed(2)}
-                              </span>
-                            </div>
-                            {/* <tr>
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+                <div className="col-md-4 col-xs-12">
+                  <div className="info-column-3 order-form">
+                    <div className="payment-information">
+                      <div className="title">
+                        <Text field={{ value: 'Payment Information:' }} tag="span" className="title_text" />
+                      </div>
+                      <div className="payment-information-price">
+                        <div>
+                          <div className="price-item">
+                            <span>Subtotal</span>
+                            <span>
+                              {price.currencySymbol}
+                              {price.subtotal.toFixed(2)}
+                            </span>
+                          </div>
+                          <div className="price-item">
+                            <span>Shipping Fees</span>
+                            <span>
+                              {price.currencySymbol}
+                              {price.shippingTotal.toFixed(2)}
+                            </span>
+                          </div>
+                          <div className="price-item">
+                            <span>Taxes</span>
+                            <span>
+                              {price.currencySymbol}
+                              {price.taxTotal.toFixed(2)}
+                            </span>
+                          </div>
+                          <div className="price-item">
+                            <span>Savings</span>
+                            <span>
+                              -{price.currencySymbol}
+                              {price.totalSavings.toFixed(2)}
+                            </span>
+                          </div>
+                          {/* <tr>
                                 <td className="tp-td1 tp-bottom-line">Total</td>
                                 <td className="tp-td2 tp-bottom-line">{price.currencySymbol}{price.total}</td>
                               </tr> */}
-                          </div>
                         </div>
-                        <div className="order-total">
-                          <div className="order-total_title">Order Total:</div>
-                          <div className="order-total_price nopadding-bottom">
-                            <span>{price.currencySymbol}</span>
-                            <span className="order-amount">{price.total.toFixed(2)}</span>
-                          </div>
+                      </div>
+                      <div className="order-total">
+                        <div className="order-total_title">Order Total:</div>
+                        <div className="order-total_price nopadding-bottom">
+                          <span>{price.currencySymbol}</span>
+                          <span className="order-amount">{price.total.toFixed(2)}</span>
                         </div>
                       </div>
                     </div>
@@ -152,7 +150,7 @@ export class Confirmation extends Jss.SafePureComponent<ConfirmationProps, Confi
               </div>
             </div>
           </div>
-        </section>
+        </div>
       </div>
     );
   }

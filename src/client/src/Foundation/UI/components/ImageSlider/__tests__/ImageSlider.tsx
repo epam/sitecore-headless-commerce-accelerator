686cbcdb5aa08999b08b6d737576bc7aea112dd3
Andrey Lavrov <Andrey_Lavrov@epam.com>
2021-04-14 15:15:09 +0000

    US1424 [P] Snapshot test for product card
    (cherry picked from commit a09f0adc96c5c07f100b3ff3fb8fac62927978f1)

---
+++ src/client/src/Foundation/UI/components/ImageSlider/__tests__/ImageSlider.tsx
@@ -0,0 +1,32 @@
+//    Copyright 2021 EPAM Systems, Inc.
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import { ImageSlider } from '../ImageSlider';
+import { Slide } from '../Slide';
+
+const fallbackImageUrl = '/-/media/HCA/Project/HCA/no-image250x150.ashx';
+
+it('ImageSlider renders correctly', () => {
+  const tree = renderer
+    .create(
+      <ImageSlider>
+        <Slide url={fallbackImageUrl} />
+      </ImageSlider>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});

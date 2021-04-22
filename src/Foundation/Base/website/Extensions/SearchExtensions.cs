a9803ae98fbb3800f3dca67686f46bc5749cf482
Mykola Hutorov <Mykola_Hutorov@epam.com>
2021-04-22 13:32:07 +0000

    US1390 [P] Add extended GraphQL search query
    (cherry picked from commit c48d69bda0efd2333de88567523978f54c33550e)

---
+++ src/Foundation/Base/website/Extensions/SearchExtensions.cs
@@ -0,0 +1,184 @@
+﻿//    Copyright 2021 EPAM Systems, Inc.
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+namespace HCA.Foundation.Base.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+
+    using GraphQL.Types;
+
+    using Sitecore.ContentSearch.Linq;
+    using Sitecore.ContentSearch.Linq.Utilities;
+    using Sitecore.Data;
+    using Sitecore.Diagnostics;
+    using Sitecore.Globalization;
+    using Sitecore.Services.Core.Extensions;
+    using Sitecore.Services.GraphQL.Content.GraphTypes;
+    using Sitecore.Services.GraphQL.Content.GraphTypes.ContentSearch;
+    using Sitecore.Services.GraphQL.GraphTypes.Connections;
+
+    public static class SearchExtensions
+    {
+        public static Expression<Func<ContentSearchResult, bool>> CreatePredicateFieldsAnd(
+            this IEnumerable<Dictionary<string, object>> fields)
+        {
+            Assert.ArgumentNotNull(fields, nameof(fields));
+
+            var predicate = PredicateBuilder.True<ContentSearchResult>();
+
+            fields.ForEach(
+                field =>
+                {
+                    var name = field["name"].ToString();
+                    var value = field["value"].ToString();
+
+                    predicate = predicate.And(result => result[name] == value);
+                });
+
+            return predicate;
+        }
+
+        public static Expression<Func<ContentSearchResult, bool>> CreatePredicateFieldsOr(
+            this IEnumerable<Dictionary<string, object>> fields)
+        {
+            Assert.ArgumentNotNull(fields, nameof(fields));
+
+            var predicate = PredicateBuilder.True<ContentSearchResult>();
+
+            fields?.ForEach(
+                field =>
+                {
+                    var name = field["name"].ToString();
+                    var value = field["value"].ToString();
+
+                    predicate = predicate.Or(result => result[name] == value);
+                });
+
+            return predicate;
+        }
+
+        public static IQueryable<ContentSearchResult> FacetOn(
+            this IQueryable<ContentSearchResult> searchQueryable,
+            IEnumerable<string> facets)
+        {
+            Assert.ArgumentNotNull(searchQueryable, nameof(searchQueryable));
+            Assert.ArgumentNotNull(facets, nameof(facets));
+
+            var queryable = searchQueryable;
+
+            facets.ForEach(facet => queryable = queryable.FacetOn(result => result[facet]));
+
+            return queryable;
+        }
+
+        public static IQueryable<ContentSearchResult> FilterByFields(
+            this IQueryable<ContentSearchResult> searchQueryable,
+            IEnumerable<Dictionary<string, object>> fieldsAnd,
+            IEnumerable<Dictionary<string, object>> fieldsOr)
+        {
+            Assert.ArgumentNotNull(searchQueryable, nameof(searchQueryable));
+            Assert.ArgumentNotNull(fieldsAnd, nameof(fieldsAnd));
+            Assert.ArgumentNotNull(fieldsOr, nameof(fieldsOr));
+
+            var predicateAnd = fieldsAnd.CreatePredicateFieldsAnd();
+            var predicateOr = fieldsOr.CreatePredicateFieldsOr();
+            var predicate = predicateAnd.And(predicateOr);
+
+            return searchQueryable.Where(predicate);
+        }
+
+        public static IQueryable<ContentSearchResult> FilterByKeyword(
+            this IQueryable<ContentSearchResult> searchQueryable,
+            string keyword)
+        {
+            Assert.ArgumentNotNull(searchQueryable, nameof(searchQueryable));
+            Assert.ArgumentNotNull(keyword, nameof(keyword));
+
+            return !string.IsNullOrWhiteSpace(keyword)
+                ? searchQueryable.Where(result => result.Content.Contains(keyword))
+                : searchQueryable;
+        }
+
+        public static IQueryable<ContentSearchResult> FilterByLanguage(
+            this IQueryable<ContentSearchResult> searchQueryable,
+            string lang)
+        {
+            Assert.ArgumentNotNull(searchQueryable, nameof(searchQueryable));
+            Assert.ArgumentNotNull(lang, nameof(lang));
+
+            return Language.TryParse(lang, out var language)
+                ? searchQueryable.Where(result => result.Language == language.Name)
+                : searchQueryable;
+        }
+
+        public static IQueryable<ContentSearchResult> FilterByRootItem(
+            this IQueryable<ContentSearchResult> searchQueryable,
+            string rootItemPath,
+            Database database)
+        {
+            Assert.ArgumentNotNull(searchQueryable, nameof(searchQueryable));
+            Assert.ArgumentNotNull(rootItemPath, nameof(rootItemPath));
+            Assert.ArgumentNotNull(database, nameof(database));
+
+            return !string.IsNullOrWhiteSpace(rootItemPath)
+                && IdHelper.TryResolveItem(database, rootItemPath, out var rootItem)
+                    ? searchQueryable.Where(result => result.AncestorIDs.Contains(rootItem.ID))
+                    : searchQueryable;
+        }
+
+        public static IQueryable<ContentSearchResult> FilterByVersion(
+            this IQueryable<ContentSearchResult> searchQueryable,
+            bool version)
+        {
+            Assert.ArgumentNotNull(searchQueryable, nameof(searchQueryable));
+            Assert.ArgumentNotNull(version, nameof(version));
+
+            return version ? searchQueryable.Where(result => result.IsLatestVersion) : searchQueryable;
+        }
+
+        public static SearchResults<ContentSearchResult> PaginateAndFinalizeResults(
+            this IQueryable<ContentSearchResult> searchQueryable,
+            ResolveFieldContext fieldContext)
+        {
+            Assert.ArgumentNotNull(searchQueryable, nameof(searchQueryable));
+            Assert.ArgumentNotNull(fieldContext, nameof(fieldContext));
+
+            return searchQueryable.ApplyEnumerableConnectionArguments(fieldContext).GetResults();
+        }
+
+        public static IQueryable<ContentSearchResult> SortResults(
+            this IQueryable<ContentSearchResult> searchQueryable,
+            string sortBy,
+            bool sortDesc)
+        {
+            Assert.ArgumentNotNull(searchQueryable, nameof(searchQueryable));
+            Assert.ArgumentNotNull(sortBy, nameof(sortBy));
+            Assert.ArgumentNotNull(sortDesc, nameof(sortDesc));
+
+            var queryable = searchQueryable;
+
+            if (!string.IsNullOrWhiteSpace(sortBy))
+            {
+                queryable = sortDesc
+                    ? searchQueryable.OrderByDescending(result => result[sortBy])
+                    : searchQueryable.OrderBy(result => result[sortBy]);
+            }
+
+            return queryable;
+        }
+    }
+}
---
+++ src/Foundation/Base/website/Services/GraphQL/Queries/ExtendedSearchQuery.cs
@@ -0,0 +1,142 @@
+﻿//    Copyright 2021 EPAM Systems, Inc.
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+namespace HCA.Foundation.Base.Services.GraphQL.Queries
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Extensions;
+
+    using global::GraphQL.Types;
+
+    using Sitecore;
+    using Sitecore.ContentSearch;
+    using Sitecore.Data.Items;
+    using Sitecore.Data.Managers;
+    using Sitecore.Diagnostics;
+    using Sitecore.Services.GraphQL.Content.GraphTypes.ContentSearch;
+    using Sitecore.Services.GraphQL.Content.Queries;
+
+    public class ExtendedSearchQuery : SearchQuery
+    {
+        protected IEnumerable<string> Facets = Enumerable.Empty<string>();
+
+        protected IEnumerable<Dictionary<string, object>> FieldsAnd = Enumerable.Empty<Dictionary<string, object>>();
+
+        protected IEnumerable<Dictionary<string, object>> FieldsOr = Enumerable.Empty<Dictionary<string, object>>();
+
+        public ExtendedSearchQuery()
+        {
+            this.AddSortArguments();
+            this.AddFieldsIncludeArguments();
+        }
+
+        protected string RootItemPath { get; set; } = Context.Site.RootPath;
+
+        protected Item RootItem => this.Database.GetItem(this.RootItemPath);
+
+        protected ISearchIndex Index { get; set; }
+
+        protected string Lang { get; set; } = Context.Language.Name ?? LanguageManager.DefaultLanguage.Name;
+
+        protected bool Version { get; set; } = true;
+
+        protected string Keyword { get; set; }
+
+        protected int After { get; set; }
+
+        protected string SortBy { get; set; }
+
+        protected bool SortDesc { get; set; }
+
+        protected virtual void Initialize(ResolveFieldContext fieldContext)
+        {
+            Assert.ArgumentNotNull(fieldContext, nameof(fieldContext));
+
+            this.RootItemPath = fieldContext.GetArgument("rootItem", this.RootItemPath);
+            this.Keyword = fieldContext.GetArgument("keyword", this.Keyword);
+            this.Lang = fieldContext.GetArgument("language", this.Lang);
+            this.Version = fieldContext.GetArgument("latestVersion", this.Version);
+
+            this.Facets = fieldContext.GetArgument("facetOn", this.Facets);
+            this.FieldsAnd = fieldContext.GetArgument("fieldsEqual", this.FieldsAnd);
+            this.FieldsOr = fieldContext.GetArgument("fieldsInclude", this.FieldsAnd);
+
+            this.SortBy = fieldContext.GetArgument("sortBy", this.SortBy);
+            this.SortDesc = fieldContext.GetArgument("sortDesc", this.SortDesc);
+
+            this.After = fieldContext.GetArgument("after", this.After);
+
+            var defaultIndexName = $"sitecore_{this.Database.Name}_index";
+            var indexName = fieldContext.GetArgument("index", defaultIndexName);
+            this.Index = ContentSearchManager.GetIndex(indexName);
+        }
+
+        protected override ContentSearchResults Resolve(ResolveFieldContext fieldContext)
+        {
+            Assert.ArgumentNotNull(fieldContext, nameof(fieldContext));
+
+            this.Initialize(fieldContext);
+
+            if (this.Index == null)
+            {
+                return new ContentSearchResults(null, 0);
+            }
+
+            using (var searchContext = this.Index.CreateSearchContext())
+            {
+                var searchQueryable = searchContext.GetQueryable<ContentSearchResult>();
+
+                searchQueryable = searchQueryable.FilterByRootItem(this.RootItemPath, this.Database);
+                searchQueryable = searchQueryable.FilterByKeyword(this.Keyword);
+                searchQueryable = searchQueryable.FilterByLanguage(this.Lang);
+                searchQueryable = searchQueryable.FilterByVersion(this.Version);
+                searchQueryable = searchQueryable.FacetOn(this.Facets);
+                searchQueryable = searchQueryable.FilterByFields(this.FieldsAnd, this.FieldsOr);
+                searchQueryable = searchQueryable.SortResults(this.SortBy, this.SortDesc);
+
+                return new ContentSearchResults(searchQueryable.PaginateAndFinalizeResults(fieldContext), this.After);
+            }
+        }
+
+        private void AddFieldsIncludeArguments()
+        {
+            var fieldsInclude = new QueryArgument<ListGraphType<ItemSearchFieldQueryValueGraphType>>
+            {
+                Name = "fieldsInclude",
+                Description = "The same as fieldsEqual but uses OR instead of AND."
+            };
+
+            this.Arguments.Add(fieldsInclude);
+        }
+
+        private void AddSortArguments()
+        {
+            var sortBy = new QueryArgument<StringGraphType>
+            {
+                Name = "sortBy",
+                Description = "Field name to sort by."
+            };
+            var sortDesc = new QueryArgument<BooleanGraphType>
+            {
+                Name = "sortDesc",
+                Description = "Sort descending or ascending."
+            };
+
+            this.Arguments.Add(sortBy);
+            this.Arguments.Add(sortDesc);
+        }
+    }
+}

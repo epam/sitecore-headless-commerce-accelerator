741f4c4d428b6b252450a5b3c94451b34dda01f7
Andrey Lavrov <Andrey_Lavrov@epam.com>
2021-05-06 09:45:55 +0000

    US1861 [P] Refactor product search component to FC
    (cherry picked from commit e6be0a3893d8d4d67be3402a60cfc86bbcf15e70)

---
+++ src/client/src/Feature/Catalog/ProductsSearch/ProductSearch.tsx
@@ -0,0 +1,81 @@
+//    Copyright 2021 EPAM Systems, Inc.
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+import React, { ChangeEvent, useCallback, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+
+import {
+  KEYWORD_PARAMETER_NAME,
+  productSearchParams,
+  productsSearchStatus,
+} from 'Feature/Catalog/Integration/ProductsSearch';
+import { LoadingStatus } from 'Foundation/Integration';
+import { ChangeRoute as changeRoute } from 'Foundation/ReactJss/SitecoreContext';
+
+import { SidebarSearch } from 'components';
+
+import { buildQueryString, getKeywordFromSearch } from './utils';
+
+export const ProductsSearch = () => {
+  const dispatch = useDispatch();
+
+  const params = useSelector(productSearchParams);
+  const status = useSelector(productsSearchStatus);
+
+  const { search, pathname } = useLocation();
+
+  const [value, setValue] = useState('');
+  const [isSearchApplied, setIsSearchApplied] = useState(false);
+
+  const loading = status === LoadingStatus.Loading;
+
+  useEffect(() => {
+    const queryValue = getKeywordFromSearch(search);
+
+    if (!search) {
+      dispatch(changeRoute(`${pathname}?${KEYWORD_PARAMETER_NAME}=`));
+    }
+
+    if (queryValue.length > 0) {
+      setValue(queryValue);
+      setIsSearchApplied(true);
+    }
+  }, [search]);
+
+  const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+    setIsSearchApplied(false);
+  }, []);
+
+  const onClear = useCallback(() => {
+    setValue('');
+    setIsSearchApplied(true);
+
+    const newSearchQueryString = buildQueryString('', params);
+
+    dispatch(changeRoute(`${pathname}${newSearchQueryString}`));
+  }, [isSearchApplied, pathname, params]);
+
+  const onSubmit = useCallback(() => {
+    if (!isSearchApplied) {
+      const newSearchQueryString = buildQueryString(value, params);
+
+      setIsSearchApplied(true);
+      dispatch(changeRoute(`${pathname}${newSearchQueryString}`));
+    }
+  }, [dispatch, params, pathname, value]);
+
+  return <SidebarSearch disabled={loading} value={value} onChange={onChange} onClear={onClear} onSubmit={onSubmit} />;
+};

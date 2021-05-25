238b0ac04ad786d3238e0d8ebc4883b4270d1b3d
Oleksandr Zaitsev <Oleksandr_Zaitsev@epam.com>
2021-05-25 15:58:53 +0000

    US2104-TA2193 [P] Add proxy interfaces for static classes
    (cherry picked from commit 5d93360609c173cf86559abe7f7da7575b798671)

---
+++ src/Foundation/Base/website/Services/MembershipService.cs
@@ -0,0 +1,25 @@
+﻿//    Copyright 2020 EPAM Systems, Inc.
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+namespace HCA.Foundation.Base.Services
+{
+    using System.Web.Security;
+    
+    public class MembershipService : IMembershipService
+    {
+        public string GetUserNameByEmail(string email) => System.Web.Security.Membership.GetUserNameByEmail(email);
+        public MembershipUser GetUser(string userName) => System.Web.Security.Membership.GetUser(userName);
+        public bool ValidateUser(string userName, string oldPassword) => System.Web.Security.Membership.ValidateUser(userName, oldPassword);
+    }
+}

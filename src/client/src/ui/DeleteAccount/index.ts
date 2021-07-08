fc60121567130672d58f888c68c288475a6a7d8d
Darya Radzevich <Darya_Radzevich@epam.com>
2021-07-08 13:37:01 +0000

    US1966-TA2988 [P] Account delete implementation

--- src/client/src/ui/DeleteAccount/index.ts
+++ src/client/src/ui/DeleteAccount/index.ts
@@ -12,37 +12,4 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
-import { renderingWithContext } from 'Foundation/ReactJss';
-import { connect } from 'react-redux';
-import { compose } from 'recompose';
-import { bindActionCreators, Dispatch } from 'redux';
-
-import * as Account from 'services/account';
-
-import Component from './Component';
-
-import { AppState, DeleteAccountDispatchProps, DeleteAccountOwnProps, DeleteAccountStateProps } from './models';
-
-const mapStateToProps = (state: AppState) => {
-  const commerceUser = Account.commerceUser(state);
-  const deleteAccountState = Account.deleteAccount(state);
-  return {
-    commerceUser,
-    deleteAccountState,
-  };
-};
-
-const mapDispatchToProps = (dispatch: Dispatch) =>
-  bindActionCreators(
-    {
-      DeleteAccount: Account.DeleteAccount,
-    },
-    dispatch,
-  );
-
-const connectedToStore = connect<DeleteAccountStateProps, DeleteAccountDispatchProps, DeleteAccountOwnProps>(
-  mapStateToProps,
-  mapDispatchToProps,
-);
-
-export const DeleteAccount = compose(connectedToStore, renderingWithContext)(Component);
+export * from './DeleteAccount';
